4a0492505e8795eb99fc4fc6cdf3f0ce70c4a205
yassa badr <75986635+yassabadr4@users.noreply.github.com>
2024-04-02 05:34:39 +0200

    create user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,3 +1,26 @@
-class UserModel{
+import 'package:api_test/core/api/end_points.dart';
 
+class UserModel {
+  final String profilePic;
+  final String email;
+  final String phone;
+  final String name;
+  final Map<String, dynamic> location;
+
+  UserModel(
+      {required this.profilePic,
+      required this.email,
+      required this.phone,
+      required this.name,
+      required this.location});
+
+  factory UserModel.fromJson(Map<String, dynamic> jsonData) {
+    return UserModel(
+      profilePic: jsonData[ApiKey.profilePic],
+      email: jsonData[ApiKey.email],
+      phone: jsonData[ApiKey.phone],
+      name: jsonData[ApiKey.name],
+      location: jsonData[ApiKey.location],
+    );
+  }
 }
